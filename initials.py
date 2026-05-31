9bfa30737e9709d001f13b2fbdb05c0e2da4517f
Bismark Odame <odamebills06@gmail.com>
2026-05-30 23:13:34 -1200

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+#def main():
+# File: <INITIALS.PY>
+# Description: <PRINTOUT MY INITIALS>
+# Assignment Number: 1
+#
+# Name: <BISMARK ODAME ASANTE>
+# STUDENT ID: <2425402310>
+# Email: <2425402310@gctu.live.edu.gh>
+# Grader: <AUGUSTUS BUCKMAN>
+#
+# On my honor, Bismark Odame Asante, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # The codes below will  print my initials(B.O.A) in stylish large letters.
+    print()
+    print("...BO")
+    print()
 
-
+    
+    print("...BBBBBBBBBBBB........OOOOOOOOOOOO........AAAAAAAAAAAA")
+    print("...BBBBBBBBBBBB........OOOOOOOOOOOO........AAAAAAAAAAAA")
+    print("...BB........BB........OO........OO........AA........AA")
+    print("...BB........BB........OO........OO........AA........AA")
+    print("...BBBBBBBBBBBB........OO........OO........AAAAAAAAAAAA")
+    print("...BBBBBBBBBBB.........OO........OO........AA........AA")
+    print("...BB........BB........OO........OO........AA........AA")
+    print("...BB........BB........OO........OO........AA........AA")
+    print("...BBBBBBBBBBBB...**...OOOOOOOOOOOO...**...AA........AA")
+    print("...BBBBBBBBBBBB...**...OOOOOOOOOOOO...**...AA........AA")
+    print()
 
 main()
